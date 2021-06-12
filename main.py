8398d3b069f93f395ef99c036595a46dd462f057
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2021-06-12 16:07:26 +0530

    requriments

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from flask import request
 # Machine Learning Imports
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
