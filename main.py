27ed1c16005ba3733f3cbc5ab2c94c8ecbcc2af7
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2021-06-12 12:11:05 +0530

    testing project setup

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def machineLearningAlog():
+
+    data = request.get_json('data')
+
+    name = data['name']
+    email = data['email']
+    success=""
+    
+    if name == "kamal" and email == "kamal@gmail.com":
+        success = 'Login Successfull'
+    else:
+        success = 'Login Failed'
+
+
+    return jsonify({success:success})
