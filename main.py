67db4b4f363b460865f23110fb9368654ae92e00
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2021-06-12 12:33:47 +0530

    i think it will work now

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-app = Flask(__name__)
+machineLearningAlog = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def machineLearningAlog():
